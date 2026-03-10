0df09fa3275eb6d9d5ec1aea2b0e17e844d356bb
Antiks <kseniya.feo2000@gmail.com>
2026-03-10 12:53:11 +0300

    Update index.js

--- notes/index.js
+++ notes/index.js
@@ -1,1 +1,26 @@
+const readline = require("readline");
 
+// обращаемся к библиотеке readline
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const NAME_PROJ = "NOTE-BOOK"
+
+let notes = [];
+
+let ste = 'Приветствую в ${NAME_PROJ}';
+
+const addNote = () => {
+	rl.question("Введите заголовок", (title) => {
+		rl.question("Напишите текст", (content) => {
+			const newNote = {
+				id: notes.length + 1,
+				title: title,
+				content: content,
+				date: new Date().toLocaleString()
+			};
+		});
+	});
+};
