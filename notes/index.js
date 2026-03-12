cdf6b158c1354c8d912ded12793ae850bfb71936
Antiks <kseniya.feo2000@gmail.com>
2026-03-12 08:42:55 +0300

    Update index.js

--- notes/index.js
+++ notes/index.js
@@ -1,26 +1,66 @@
-const readline = require("readline");
+const readline = require("readline"); // импортируем модуль из node
 
-// обращаемся к библиотеке readline
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-const NAME_PROJ = "NOTE-BOOK"
+const NAME_PROJ = '"NOTE"-"BOOK"';
 
 let notes = [];
 
-let ste = 'Приветствую в ${NAME_PROJ}';
+let str = `Тебя приветствует приложение ${NAME_PROJ}`;
 
 const addNote = () => {
-	rl.question("Введите заголовок", (title) => {
-		rl.question("Напишите текст", (content) => {
-			const newNote = {
-				id: notes.length + 1,
-				title: title,
-				content: content,
-				date: new Date().toLocaleString()
-			};
-		});
-	});
+  rl.question("Введите заголовок", (title) => {
+    rl.question("Напишите текст заметки", (content) => {
+      const newNote = {
+        id: notes.length + 1,
+        title: title,
+        content: content,
+        date: new Date().toLocaleString()
+      };
+      notes.push(newNote);
+      console.log(`Заметка ${newNote.title} сохранена!`);
+      console.log(`Всего заметок ${notes.length}`);
+
+      showMenu();
+    });
+  });
+}; 
+
+const showNotes = () => {
+  consol.log("----Все ваши заметки----");
+  notes.forEach((note) => {
+    console.log("-".repeat(30));
+    console.log(`${note.id} * ${note.date}`);
+    console.log(`${note.title}`);
+    console.log(`${note.content}`);
+    console.log("-".repeat(30));
+  });
+  showMenu();
 };
+
+const showMenu = () => {
+  console.log(`${str}`);
+  console.log(`Всего заметок ${notes.length}`);
+  console.log("Главное меню");
+  console.log("1. Доюавить заметку");
+  console.log("2. Посмотреть заметки");
+
+  rl.question("Выберите пункт от 1 до 2", (choice) => {
+    switch(choice){
+      case '1':
+        addNote();
+        break;
+      case '2':
+        showNotes();
+        break;
+      default:
+        console.log("Нет такого пункта!");
+        showMenu();
+    };
+  });
+};
+
+showMenu();
