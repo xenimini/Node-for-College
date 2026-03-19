7ddb5b3f1987c11247a827623b20d9eee049eaa4
xenimini <kseniya.feo2000@gmail.com>
2026-03-19 09:05:40 +0300

    Update server.js

--- notes/server.js
+++ notes/server.js
@@ -1,3 +1,11 @@
-const http = require('http');
-const fs = require('http');
+const http = require('http'); // поднять сервер
+const fs = require('fs'); // заголовки
+const path = require('path'); // определение пути к файлу
+ 
+const helper = require("./utils/helper"); // импортируем свои модули
+const helper = require("./utils/helper"); // импортируем свои модули
 
+let notes = []
+(async => {
+  
+})();
